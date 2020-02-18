115e52c7095e108529ec7723504a2550b11ae71b
Hsin Ling Hu <hsinkava11@gmail.com>
2020-02-18 09:17:08 -0800

    second drill

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ app.get('/sum', (req, res) =>{
   res.send(`The sum of a and b is ${a + b}`); 
 });
 
+app.get('/cipher', (req, res) =>{
+  const  text= req.query.text.toUpperCase();
+  
+  const shift = Number(req.query.shift);
+
+  let sum ='';
+  
+  for(let i=0; i< text.length; i++){
+
+    sum += String.fromCharCode(text.charCodeAt(i)+shift);
+    
+  }
+  res.send(sum);
+
+
+  
+});
+
 app.listen(8000, () => {
   console.log('Express is listening on 8000');
-})
+});
